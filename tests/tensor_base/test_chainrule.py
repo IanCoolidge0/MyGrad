17fc95436aeac738ea79b81e3ce354d950a81f42
Ryan Soklaski <ry26099@mit.edu>
2018-07-27 15:00:55 -0400

    building a test

--- tests/tensor_base/test_chainrule.py
+++ tests/tensor_base/test_chainrule.py
@@ -26,3 +26,29 @@ def test_chainrule_scalar(x, y, z):
     assert_allclose(y.grad, 2 * z.data * f.data * x.data)
     assert_allclose(z.grad, f.data**2 + z.data * 2 * f.data)
     #assert w.grad is None
+
+
+# def test_chainrule_scalar2(x, y, z):
+#     x1, x2 = (Tensor(2.), Tensor(3.))
+#     W = Tensor(2.3)
+#     V = Tensor(4.2)
+#     h1 = Tensor(-1.3)
+#
+#     h2 = (x1 * W + h1 * V)
+#     h2 = (x2 * W + h2 * V)
+#
+#
+#
+#
+#     f = x*y + z
+#     g = x + z*f*f
+#
+#     # # check side effects
+#     # unused = 2*g - f
+#     # w = 1*f
+#
+#     g.backward()
+#     assert_allclose(f.grad, 2 * z.data * f.data)
+#     assert_allclose(x.grad, 1 + 2 * z.data * f.data * y.data)
+#     assert_allclose(y.grad, 2 * z.data * f.data * x.data)
+#     assert_allclose(z.grad, f.data**2 + z.data * 2 * f.data)
